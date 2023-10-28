528b0f8330286099221ac460f9bf464026e09349
Ramal Abbassov <ramalabbasov044@gmail.com>
2023-10-28 22:23:25 +0400

    add

--- src/components/Statics/Battery/index.jsx
+++ src/components/Statics/Battery/index.jsx
@@ -1,17 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect , useState } from 'react';
 import styles from './style.module.css'
 const BatteryComponent = () => {
     const [battery, setBattery] = useState();
 
+
     navigator.getBattery().then((battery) => {
-        let batteryToFaiz = battery.level * 100
-        setBattery(batteryToFaiz)
-    });
+      let batteryToFaiz = battery.level * 100
+      setBattery(batteryToFaiz)
+    })
+
+
   return (
     <>
         <div className={styles.batteryBody}>
             <div style={{width:battery+"%"}} className={styles.batteryColor}>
-                
+              <p className={styles.batteryCount}>
+                {
+                  battery
+                }
+              </p>
             </div>
         </div>
     </>
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,75 +4,79 @@ import styles from './style.module.css'
 import Header from '../../components/Statics/Header/index'
 
 const Home = () => {
-  const [value,setValue] = useState(0)
-  const [totValue,setTotValue] = useState([])
+  const [value, setValue] = useState(0)
+  const [totValue, setTotValue] = useState([])
 
   const showScreen = (num) => {
-      setValue(num)
-      setTotValue([...totValue,num])
+    setValue(num)
+    setTotValue([...totValue, num])
   }
 
-  const clearScreen = () => {
+  let clearScreen = () => {
     setTotValue([])
   }
 
-  const resultScreen = () => {
-      let index = totValue.indexOf("*")
+  let removeLastElement = () => {
+    if (totValue.length > 0) {
+      let umumiTotval = [...totValue];
+      // console.log(umumiTotval+"asd");
 
-      let firstArr = totValue.slice(0,index)
-      let number1 = parseInt(firstArr.join(''));
-
-      let lastItem = totValue.at(-1)
-      let lastArr = totValue.slice(index+1,lastItem)
-      let number2 = parseInt(lastArr.join(""))
-      console.log(number2);
-
-      setTotValue([number1 * number2]);
+      setTotValue(umumiTotval.pop());
+    }
+    console.log("err");
   }
 
+  let randomOperator = () => {
+      let arr = ["-","+"]
+      let i = Math.floor(Math.random() * 2)
+      showScreen(arr[i])
+  }
 
-  
+  const resultScreen = () => {
+      const result = eval(totValue.join(''))
+      setTotValue([result.toFixed(2)])
+  }
 
   return (
     <>
-        <div className={styles.screen}>
-            <Header />
+      <div className={styles.screen}>
+        <Header />
+        
+        <Screen number={totValue} />
 
-            <Screen number={totValue} />
+        {/* Buttons */}
+          <div className={styles.buttonGroup}>
+            {/* Group 1 */}
+              <button onClick={() => clearScreen()} className={styles.clearButton}>C</button>
+              <button onClick={() => showScreen('%')} className={styles.operator}>%</button>
+              <button onClick={() => showScreen('/')} className={styles.operator}>/</button>
+              <button onClick={removeLastElement} className={styles.operator}>Del</button>
 
-            {/* Buttons */}
-            <div className={styles.buttonGroup}>
-              {/* Group 1 */}
-                  <button onClick={() => clearScreen()} className={styles.clearButton}>C</button>
-                  <button className={styles.operator}>()</button>
-                  <button className={styles.operator}>%</button>
-                  <button onClick={() => showScreen("/")} className={styles.operator}>/</button>
+            {/* Group 2 */}
+              <button onClick={() => showScreen(7)} className={styles.numberButton}>7</button>
+              <button onClick={() => showScreen(8)} className={styles.numberButton}>8</button>
+              <button onClick={() => showScreen(9)} className={styles.numberButton}>9</button>
+              <button onClick={() => showScreen('*')} className={styles.operator}>*</button>
 
-              {/* Group 2 */}
-                  <button onClick={() => showScreen(7)} className={styles.numberButton}>7</button>
-                  <button onClick={() => showScreen(8)} className={styles.numberButton}>8</button>
-                  <button onClick={() => showScreen(9)} className={styles.numberButton}>9</button>
-                  <button onClick={() => showScreen("*")} className={styles.operator}>*</button>
+            {/* Group 3 */}
+              <button onClick={() => showScreen(4)} className={styles.numberButton}>4</button>
+              <button onClick={() => showScreen(5)} className={styles.numberButton}>5</button>
+              <button onClick={() => showScreen(6)} className={styles.numberButton}>6</button>
+              <button onClick={() => showScreen('-')} className={styles.operator}>-</button>
 
-              {/* Group 3 */}
-                  <button onClick={() => showScreen(4)} className={styles.numberButton}>4</button>
-                  <button onClick={() => showScreen(5)} className={styles.numberButton}>5</button>
-                  <button onClick={() => showScreen(6)} className={styles.numberButton}>6</button>
-                  <button onClick={() => showScreen("-")} className={styles.operator}>-</button> 
+            {/* Group 4 */}
+              <button onClick={() => showScreen(1)} className={styles.numberButton}>1</button>
+              <button onClick={() => showScreen(2)} className={styles.numberButton}>2</button>
+              <button onClick={() => showScreen(3)} className={styles.numberButton}>3</button>
+              <button onClick={() => showScreen('+')} className={styles.operator}>+</button>
 
-              {/* Group 4 */}
-                <button onClick={() => showScreen(1)} className={styles.numberButton}>1</button>
-                <button onClick={() => showScreen(2)} className={styles.numberButton}>2</button>
-                <button onClick={() => showScreen(3)} className={styles.numberButton}>3</button>
-                <button onClick={() => showScreen("+")} className={styles.operator}>+</button>
-
-              {/* Group 5 */}
-                <button  className={styles.numberButton}>+/-</button>
-                <button onClick={() => showScreen(0)} className={styles.numberButton}>0</button>
-                <button className={styles.numberButton}>.</button>
-                <button onClick={() => resultScreen()} className={styles.resultOperator}>=</button>
-            </div>
-        </div>
+            {/* Group 5 */}
+              <button onClick={randomOperator} className={styles.numberButton}>+/-</button>
+              <button onClick={() => showScreen(0)} className={styles.numberButton}>0</button>
+              <button onClick={() => showScreen(".")} className={styles.numberButton}>.</button>
+              <button onClick={() => resultScreen()} className={styles.resultOperator}>=</button>
+          </div>
+      </div>
     </>
   )
 }
